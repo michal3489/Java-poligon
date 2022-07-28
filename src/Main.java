41aa95f345daff82ef8addcba7db9bec41c5ec27
michal3489 <61480643+michal3489@users.noreply.github.com>
2022-07-28 19:33:25 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        {
+            int a, b;
+            Scanner scanner = new Scanner(System.in);
+            a = scanner.nextInt();
+
+            if(a % 2 == 0){
+
+
+            }
+
+        }
+
+
+    }
+}
