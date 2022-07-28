1f752880cb3e57a25cbf656962c46f07b20269e2
michal3489 <61480643+michal3489@users.noreply.github.com>
2022-07-28 20:26:45 +0100

    czy parzysta
    Nie mam pojęcia czemu nie działa

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        {
             int a, b;
             Scanner scanner = new Scanner(System.in);
             a = scanner.nextInt();
 
             if(a % 2 == 0){
-
+                System.out.println("Prawda");
 
             }
+            else {
+                System.out.println("Fałsz");
+            }
 
-        }
+
 
 
     }
